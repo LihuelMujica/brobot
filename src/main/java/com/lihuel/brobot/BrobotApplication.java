3391cb14f10261072831829d80dc00dbee20f84d
lihuelmujica <lihuelmujica@gmail.com>
2023-07-16 16:06:27 -0300

    feat: Add Feign client to main application
    - Include Feign client dependencies in the project configuration
    - Create Feign configuration class to customize Feign client behavior
    - Add Feign client interface for communicating with external services
    
    This commit introduces Feign to the main application, allowing communication with external services. Feign client dependencies are added to the project, and a configuration class is created to customize Feign client behavior. Additionally, a Feign client interface is defined to define the endpoints and methods for interacting with the external services.

--- src/main/java/com/lihuel/brobot/BrobotApplication.java
+++ src/main/java/com/lihuel/brobot/BrobotApplication.java
@@ -2,8 +2,10 @@ package com.lihuel.brobot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BrobotApplication {
 
     public static void main(String[] args) {
