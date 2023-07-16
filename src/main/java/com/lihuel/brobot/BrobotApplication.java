b47bf000b1773bd6bc8c197978904a949f37ac40
lihuelmujica <lihuelmujica@gmail.com>
2023-07-16 16:05:32 -0300

    chore: Update application.properties
    - Add server.port property to specify the server port, with a default value of 8080 if not provided
    - Configure database related properties, including the URL, database name, username, password, and driver class name
    - Enable debug logging for org.springframework.security package
    - Set spring.jpa.show-sql property to true to show SQL statements
    - Set spring.jpa.properties.hibernate.format_sql property to true to format SQL queries
    - Set feign.client.config.default.loggerLevel property to full to enable full logging for Feign client requests
    
    These changes in the application.properties file update the server port, database configuration, logging levels, and Feign client logging settings.

---
+++ src/main/java/com/lihuel/brobot/BrobotApplication.java
@@ -0,0 +1,13 @@
+package com.lihuel.brobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrobotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lihuel/brobot/BrobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lihuel.brobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
