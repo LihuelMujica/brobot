431f5d6dd87eed27028f2076ac482c18b82ad807
lihuelmujica <lihuelmujica@gmail.com>
2023-07-16 18:26:49 -0300

    feat: Add Command interface for bot commands
    - Create Command interface with methods for name, description, options, and execution
    - Define getName() method to retrieve the command name
    - Define getDescription() method to retrieve the command description
    - Define getOptions() method to retrieve the command options
    - Define execute() method to execute the command logic based on SlashCommandInteractionEvent
    
    This commit introduces the Command interface, which provides the contract for bot commands. It defines methods to retrieve the command name, description, options, and execute the command logic based on the SlashCommandInteractionEvent. Implementations of this interface will provide the specific behavior for each command.

---
+++ src/main/java/com/lihuel/brobot/discord/commands/Command.java
@@ -0,0 +1,19 @@
+package com.lihuel.brobot.discord.commands;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.List;
+
+public interface Command {
+
+    String getName();
+
+    String getDescription();
+
+    List<OptionData> getOptions();
+
+    void execute(SlashCommandInteractionEvent event);
+
+
+}
