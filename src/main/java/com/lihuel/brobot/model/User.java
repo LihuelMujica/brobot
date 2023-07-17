bb2d230f3adce4def0718eaff52da806d02c27ea
lihuelmujica <lihuelmujica@gmail.com>
2023-07-16 23:42:16 -0300

    feat: Add User entity class
    - Create User class with JPA annotations for entity mapping
    - Define fields for id, discordId, and steamUserId
    - Add appropriate annotations for primary key, column names, and generated value
    
    This commit introduces the User entity class, which represents a user in the system. The class is annotated with JPA annotations for entity mapping to the corresponding database table. It includes fields for id, discordId, and steamUserId. The id field is annotated as the primary key with auto-generation. The discordId and steamUserId fields store the respective user identifiers.

---
+++ src/main/java/com/lihuel/brobot/model/User.java
@@ -0,0 +1,16 @@
+package com.lihuel.brobot.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    private String discordId;
+    private String steamUserId;
+}
