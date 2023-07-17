4cfe1e6d818c694f1ab72c07ad67b3a2b826e219
lihuelmujica <lihuelmujica@gmail.com>
2023-07-16 23:48:29 -0300

    test: Add UserRepositoryTest for UserRepository CRUD operations

---
+++ src/test/java/com/lihuel/brobot/repository/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.lihuel.brobot.repository;
+
+import com.lihuel.brobot.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void crud() {
+        User user = new User();
+        user.setDiscordId("123");
+        user.setSteamUserId("123");
+        userRepository.save(user);
+
+        User user2 = userRepository.findById(user.getId()).orElse(null);
+
+        assertNotNull(user2);
+        assertEquals(user.getDiscordId(), user2.getDiscordId());
+        assertEquals(user.getSteamUserId(), user2.getSteamUserId());
+
+        userRepository.delete(user);
+    }
+
+}
