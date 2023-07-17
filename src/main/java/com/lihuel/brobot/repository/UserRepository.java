133f2ba5f55e7f14f2b21731f383fdd2a2bf1337
lihuelmujica <lihuelmujica@gmail.com>
2023-07-16 23:48:58 -0300

    feat: Add UserRepository interface

---
+++ src/main/java/com/lihuel/brobot/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lihuel.brobot.repository;
+
+import com.lihuel.brobot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
