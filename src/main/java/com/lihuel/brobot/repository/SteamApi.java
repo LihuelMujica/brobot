7efe3b1e21f47d9813f6a708bda97fe2ba68fe79
lihuelmujica <lihuelmujica@gmail.com>
2023-07-16 23:29:15 -0300

    feat: Add getSteamProfile method and related API integration
    - Define SteamDevAPI interface with @RequestLine for retrieving Steam profile information
    - Create steamDevAPI instance using Feign.builder() and SteamDevApiDecoder
    - Implement getSteamProfile method to retrieve Steam profile using the SteamDevAPI
    - Pass the steamid and STEAM_KEY as parameters to the API call
    - Handle the SteamApiException and verify the response contains at least one player
    - Return the first player from the SteamProfileDTO response
    
    This commit adds the necessary functionality to retrieve a Steam profile using the getSteamProfile method in the SteamApi class. The SteamDevAPI interface is defined with the appropriate @RequestLine for making the API call. The steamDevAPI instance is created using Feign.builder() and SteamDevApiDecoder. The getSteamProfile method makes the API call with the steamid and STEAM_KEY as parameters, handles the SteamApiException, and retrieves the player data from the SteamProfileDTO response.

--- src/main/java/com/lihuel/brobot/repository/SteamApi.java
+++ src/main/java/com/lihuel/brobot/repository/SteamApi.java
@@ -1,7 +1,9 @@
 package com.lihuel.brobot.repository;
 
 import com.lihuel.brobot.dto.SteamGameDTO;
+import com.lihuel.brobot.dto.SteamProfileDTO;
 import com.lihuel.brobot.exception.SteamApiException;
+import com.lihuel.brobot.repository.decoder.steam.SteamDevApiDecoder;
 import com.lihuel.brobot.repository.decoder.steam.SteamStoreDecoder;
 import feign.Feign;
 import feign.FeignException;
@@ -21,12 +23,27 @@ public class SteamApi {
         Map<String, SteamGameDTO> getAppDetails(@Param("appid") String appid);
     }
 
+    private interface SteamDevAPI {
+        @RequestLine("GET /ISteamUser/GetPlayerSummaries/v0002/?key={key}&steamids={steamid}&format=json")
+        SteamProfileDTO getSteamProfile(@Param("steamid") String steamid, @Param("key") String key);
+
+
+    }
+
 
     private final SteamStoreApi steamStoreApi = Feign.builder().decoder(new SteamStoreDecoder()).target(SteamStoreApi.class, "https://store.steampowered.com");
+
+    private final SteamDevAPI steamDevAPI = Feign.builder().decoder(new SteamDevApiDecoder()).target(SteamDevAPI.class, "https://api.steampowered.com");
 
     public SteamGameDTO getAppDetails(String appid) throws FeignException, SteamApiException {
         Map<String, SteamGameDTO> appdetails =  steamStoreApi.getAppDetails(appid);
         if (appdetails.get(appid).getData() == null) throw new SteamApiException("App not found");
         return appdetails.get(appid);
+    }
+
+    public SteamProfileDTO.Player getSteamProfile(String steamid) throws FeignException, SteamApiException {
+        SteamProfileDTO steamProfileDTO = steamDevAPI.getSteamProfile(steamid, STEAM_KEY);
+        if (steamProfileDTO.getResponse().getPlayers().length == 0) throw new SteamApiException("User not found");
+        return steamProfileDTO.getResponse().getPlayers()[0];
     }
 }
