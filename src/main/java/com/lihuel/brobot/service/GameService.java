93b82ff997be96199e1bf9f6afb839dd7baa3a35
lihuelmujica <lihuelmujica@gmail.com>
2023-07-19 11:32:34 -0300

    feat: add findGamesByDiscordId and findGamesInCommon methods to GameService

--- src/main/java/com/lihuel/brobot/service/GameService.java
+++ src/main/java/com/lihuel/brobot/service/GameService.java
@@ -3,7 +3,13 @@ package com.lihuel.brobot.service;
 import com.lihuel.brobot.exception.SteamApiException;
 import com.lihuel.brobot.model.Game;
 
+import java.util.List;
+
 public interface GameService {
 
     public Game save(String steamUrl) throws SteamApiException;
+    public List<Game> findGamesByDiscordId(String discordId) throws SteamApiException;
+
+    public List<Game> findGamesInCommon(List<String> discordIds) throws SteamApiException;
+
 }
--- src/main/java/com/lihuel/brobot/service/impl/GameServiceImpl.java
+++ src/main/java/com/lihuel/brobot/service/impl/GameServiceImpl.java
@@ -1,13 +1,22 @@
 package com.lihuel.brobot.service.impl;
 
 import com.lihuel.brobot.dto.SteamGameDTO;
+import com.lihuel.brobot.dto.SteamOwnedGameDTO;
 import com.lihuel.brobot.exception.SteamApiException;
 import com.lihuel.brobot.model.Game;
+import com.lihuel.brobot.model.User;
 import com.lihuel.brobot.repository.GameRepository;
 import com.lihuel.brobot.repository.SteamApi;
+import com.lihuel.brobot.repository.UserRepository;
 import com.lihuel.brobot.service.GameService;
 import com.lihuel.brobot.utils.SteamURLUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class GameServiceImpl implements GameService {
@@ -15,10 +24,12 @@ public class GameServiceImpl implements GameService {
     private final SteamApi steamApi;
 
     private final GameRepository gameRepository;
+    private final UserRepository userRepository;
 
-    public GameServiceImpl(SteamApi steamApi, GameRepository gameRepository) {
+    public GameServiceImpl(SteamApi steamApi, GameRepository gameRepository, UserRepository userRepository) {
         this.steamApi = steamApi;
         this.gameRepository = gameRepository;
+        this.userRepository = userRepository;
     }
 
 
@@ -35,4 +46,36 @@ public class GameServiceImpl implements GameService {
 
         return gameRepository.save(game);
     }
+
+    @Override
+    public List<Game> findGamesByDiscordId(String discordId) throws SteamApiException {
+        User user = userRepository.findById(discordId).orElseThrow(() -> new IllegalArgumentException("No se ha encontrado el usuario"));
+        Set<SteamOwnedGameDTO.Game> ownedGamesDTO = steamApi.getOwnedGames(user.getSteamUserId());
+        return gameRepository.findAllById(ownedGamesDTO.stream().map(SteamOwnedGameDTO.Game::getAppId).toList());
+    }
+
+    @Override
+    public List<Game> findGamesInCommon(List<String> discordIds) throws SteamApiException {
+        List<User> users = userRepository.findAllById(discordIds);
+        Set<String> commonGamesIds = new HashSet<>();
+        HashMap<String, Set<String>> ownedGamesIds = new HashMap<>();
+
+        for (User user : users) {
+            ownedGamesIds.put(user.getDiscordId(), steamApi.getOwnedGames(user.getSteamUserId()).stream().map(SteamOwnedGameDTO.Game::getAppId).collect(Collectors.toSet()));
+        }
+
+        for (User user : users) {
+            if (commonGamesIds.size() == 0) {
+                commonGamesIds.addAll(ownedGamesIds.get(user.getDiscordId()));
+            } else {
+                commonGamesIds.retainAll(ownedGamesIds.get(user.getDiscordId()));
+            }
+        }
+
+        return gameRepository.findAllById(commonGamesIds);
+
+    }
+
+
+
 }
