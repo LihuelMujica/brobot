e5d9884330b20e45cff987d6d879f667a3e9bb4c
lihuelmujica <lihuelmujica@gmail.com>
2023-07-21 03:34:42 -0300

    test: add findBySteamIdAndHasSteamPlayTogetherIsTrue and findByHasPiratedMultiplayerIsTrue to GameRepository

--- src/test/java/com/lihuel/brobot/repository/GameRepositoryTest.java
+++ src/test/java/com/lihuel/brobot/repository/GameRepositoryTest.java
@@ -20,9 +20,12 @@ class GameRepositoryTest {
     @BeforeEach
     void setUp() {
         List<Game> games = List.of(
-          new Game(),
-          new Game(),
-          new Game()
+            new Game(),
+            new Game(),
+            new Game(),
+            new Game(),
+            new Game(),
+            new Game()
         );
 
         games.get(0).setName("test1");
@@ -37,6 +40,21 @@ class GameRepositoryTest {
         games.get(2).setHasPiratedMultiplayer(true);
         games.get(2).setSteamId("test3");
         games.get(2).setSteamUrl("https://store.steampowered.com/app/3");
+        games.get(3).setName("test4");
+        games.get(3).setHasPiratedMultiplayer(false);
+        games.get(3).setHasSteamPlayTogether(true);
+        games.get(3).setSteamId("test4");
+        games.get(3).setSteamUrl("https://store.steampowered.com/app/4");
+        games.get(4).setName("test5");
+        games.get(4).setHasPiratedMultiplayer(true);
+        games.get(4).setHasSteamPlayTogether(true);
+        games.get(4).setSteamId("test5");
+        games.get(4).setSteamUrl("https://store.steampowered.com/app/5");
+        games.get(5).setName("test6");
+        games.get(5).setHasPiratedMultiplayer(false);
+        games.get(5).setHasSteamPlayTogether(true);
+        games.get(5).setSteamId("test6");
+        games.get(5).setSteamUrl("https://store.steampowered.com/app/6");
 
         gameRepository.saveAll(games);
 
@@ -44,7 +62,7 @@ class GameRepositoryTest {
 
     @AfterEach
     void tearDown() {
-        gameRepository.deleteAll(gameRepository.findAllById(List.of("test1", "test2", "test3")));
+        gameRepository.deleteAll(gameRepository.findAllById(List.of("test1", "test2", "test3", "test4", "test5", "test6")));
     }
 
     @Test void crud() {
@@ -83,6 +101,41 @@ class GameRepositoryTest {
         assertEquals("https://store.steampowered.com/app/1", games.get(0).getSteamUrl());
         assertEquals("https://store.steampowered.com/app/2", games.get(1).getSteamUrl());
         assertEquals("https://store.steampowered.com/app/3", games.get(2).getSteamUrl());
+    }
+
+    @Test
+    void findBySteamIdInAndHasSteamPlayTogetherIsTrue() {
+        List<Game> games = gameRepository.findBySteamIdInAndHasSteamPlayTogetherIsTrue(List.of("test4", "test5", "test6"));
+        assertEquals(3, games.size());
+        assertEquals("test4", games.get(0).getName());
+        assertEquals("test5", games.get(1).getName());
+        assertEquals("test6", games.get(2).getName());
+        assertEquals(false, games.get(0).getHasPiratedMultiplayer());
+        assertEquals(true, games.get(1).getHasPiratedMultiplayer());
+        assertEquals(false, games.get(2).getHasPiratedMultiplayer());
+        assertEquals(true, games.get(0).getHasSteamPlayTogether());
+        assertEquals(true, games.get(1).getHasSteamPlayTogether());
+        assertEquals(true, games.get(2).getHasSteamPlayTogether());
+        assertEquals("https://store.steampowered.com/app/4", games.get(0).getSteamUrl());
+        assertEquals("https://store.steampowered.com/app/5", games.get(1).getSteamUrl());
+        assertEquals("https://store.steampowered.com/app/6", games.get(2).getSteamUrl());
+    }
+
+    @Test
+    void findByHasPiratedMultiplayerIsTrue() {
+        List<Game> games = gameRepository.findByHasPiratedMultiplayerIsTrue();
+        games = games.stream().filter(game -> game.getSteamId().contains("test")).toList();
+
+        assertEquals(3, games.size());
+        assertEquals("test1", games.get(0).getName());
+        assertEquals("test3", games.get(1).getName());
+        assertEquals("test5", games.get(2).getName());
+        assertEquals(true, games.get(0).getHasPiratedMultiplayer());
+        assertEquals(true, games.get(1).getHasPiratedMultiplayer());
+        assertEquals(true, games.get(2).getHasPiratedMultiplayer());
+        assertEquals("https://store.steampowered.com/app/1", games.get(0).getSteamUrl());
+        assertEquals("https://store.steampowered.com/app/3", games.get(1).getSteamUrl());
+        assertEquals("https://store.steampowered.com/app/5", games.get(2).getSteamUrl());
     }
 
 }
